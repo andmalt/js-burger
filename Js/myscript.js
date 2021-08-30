f14be6f733426a5ad77d9318eca6fc51e293a1fb
andmalt <andrea.maltempi@gmail.com>
2021-08-30 22:15:25 +0200

    inserito su js sconto panino

--- Js/myscript.js
+++ Js/myscript.js
@@ -6,18 +6,22 @@ let nomePanino = document.getElementById("nome-panino");
 
 let bottoneCalcolo = document.getElementById("bottone-calc");
 
+let prezzoInizialePanino;
 
 
 let somma = 0;
 
 bottoneCalcolo.addEventListener("click",
 function(){
-    if(nomePanino.length == 0){
+    if(nomePanino.value.length == 0){
         alert("Inserire nome panino");
     }else{
-        let prezzoInizialePanino = 50;
+        prezzoInizialePanino = 50;
+
         let ingredientiAggiunti = document.getElementsByClassName("checkbox-ingredienti");
 
+        let couponSconto = document.getElementById("coupon-sconto");
+        console.log("coupon-sconto: "+ couponSconto);
 
         for(let i = 0; i < ingredientiAggiunti.length; i++){
             let ingrediente = ingredientiAggiunti[i]; 
@@ -26,19 +30,24 @@ function(){
 
             if(ingrediente.checked === true){
                 prezzoInizialePanino += 7.5;
-            }
-
-            // sconto panino
-            let couponSconto = document.getElementById("coupon-sconto");
-            
-            console.log(prezzoInizialePanino);
-            
+            }   
         }
+
+        // sconto panino
+
+        if(couponSconto.value.length != 0){
+            if(listaCoupon.includes(couponSconto.value)){
+                prezzoInizialePanino = prezzoInizialePanino * 0.8;
+            }else{
+                alert("Coupon non valido!!!");
+            }          
+        }      
+        console.log(prezzoInizialePanino);
         somma = prezzoInizialePanino;
         document.getElementById("prezzo-panino").innerHTML = somma +"&euro;";
     }
 }
 );
-console.log(somma);
+
 
 
